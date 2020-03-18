b9466144e8d6e71894dc8e3793a2d5bbb81e1dd8
MatiasDaniel <matias.daniel.huentian@gmail.com>
2020-03-18 16:47:26 -0400

    capitulo 10 practica sobre como ocupar javascript para poder ejecutar funciones mediante botones

--- intro.js
+++ intro.js
@@ -1,1 +1,7 @@
 
+// function suma(){
+//     var num1  =     parseFloat(document.getElementById(num1).value);
+//     var num2  =     parseFloat(document.getElementById(num2).value);
+
+//     return num1+num2;
+// }
