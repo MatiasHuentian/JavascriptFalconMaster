013e753fac3d0821068ce379a7e7cd76573f5180
MatiasDaniel <matias.daniel.huentian@gmail.com>
2020-03-18 12:48:37 -0400

    capitlo 6 condicional if (ya conocida)

--- intro.js
+++
@@ -1,7 +0,0 @@
-var array  = ["item0" , "item1" , "item2" , "item3" , "item4"];
-var array2 = ["itema","itemb","itemc","itemd","iteme",]
-
-
-var array3 = array.concat("holaa" , "hlaa2" , "hola 33333");
-
-document.write(array3);
