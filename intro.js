7f2a52f2c48876068f479e03f4210e7eaf75e110
MatiasDaniel <matias.daniel.huentian@gmail.com>
2020-03-18 20:13:36 -0400

    Capitulo 14 crear e insertar nodos al DOM

--- intro.js
+++ intro.js
@@ -1,4 +1,19 @@
-var parrafosP = document.getElementsByTagName("p");
-var parrafoDos = document.getElementById("segundo").innerHTML;
+//Crear nodos de DOM
 
-document.write(parrafoDos);
+//1.- Crear el elemento (crear un elemento p , b, etc)
+//2.- Crear un nodo de texto (crear el texto que irá dentro de aquel parrafo)
+//3.- Añadir el noo de texto al elemento (unir el texto con el elemento)
+//4.- Agregar atributos al elemento (embellezer el elemento)
+//5.- agregar el elemento al documento (insertar el elemento)
+
+
+//1.- Crear el elemento (crear un elemento p , b, etc)
+var elemento = document.createElement("h2");
+//2.- Crear un nodo de texto (crear el texto que irá dentro de aquel parrafo)
+var contenido =document.createTextNode("Este es el titular del h2")
+//3.- Añadir el noo de texto al elemento (unir el texto con el elemento)
+elemento.appendChild(contenido);
+//4.- Agregar atributos al elemento (embellezer el elemento)
+elemento.setAttribute("align" , 'center');
+//5.- agregar el elemento al documento (insertar el elemento)
+document.getElementById("subtitulo").appendChild(elemento);
